0bd80ba45b831a1da41b404c5a203a2ec9d658e3
Bartlomiej Drozd <drozd.bartek96@gmail.com>
2024-06-27 15:59:48 +0200

    init commit

---
+++ MemberStorage.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <unordered_map>
+#include <mutex>
+#include <stdexcept>
+#include <typeinfo>
+#include <algorithm>
+
+struct MemberTypeBase
+{
+    MemberTypeBase(size_t hash) : class_hash(hash) {}
+    virtual bool isEqual(const MemberTypeBase* rhs) const = 0;
+    size_t class_hash;
+};
+
+template <typename _ClassType, typename _MemberType>
+struct Member_t : MemberTypeBase
+{
+    Member_t(_MemberType _ClassType::*arg)
+        : member(arg), MemberTypeBase(typeid(_ClassType).hash_code())
+    {
+    }
+    _MemberType _ClassType::*member;
+
+    bool isEqual(const MemberTypeBase* rhs) const override
+    {
+        if(class_hash == rhs->class_hash)
+        {
+            auto* ref =
+                static_cast<const Member_t<_ClassType, _MemberType>*>(rhs);
+            if (ref)
+                return ref->member == member;
+        }
+        return false;
+    }
+};
+
+//! @brief member storage base class used to register members. 
+struct MemberStorageBase
+{
+public:
+    //! @brief call this method in derived constructor
+    //! @tparam T 
+    //! @param  
+    template <typename T>
+    void callInitMembers(const T*)
+    {
+        static std::once_flag register_once;
+        std::call_once(register_once, &MemberStorageBase::InitMembers, this);
+    }
+
+    virtual void InitMembers() = 0;
+
+protected:
+    class MemberHasher
+    {
+    public:
+        size_t operator()(const MemberTypeBase* val) const
+        {
+            return reinterpret_cast<uintptr_t>(val);
+        }
+    };
+
+    class MemberEqual
+    {
+    public:
+        bool operator()(const MemberTypeBase* val1,
+                        const MemberTypeBase* val2) const
+        {
+            return val1->isEqual(val2);
+        }
+    };
+};
+
+//! @brief simple storage class to associate StoredType value for every registered data member. Works like a singleton storage for T type
+//! @tparam T 
+//! @tparam StoredType 
+template <typename T, typename StoredType>
+struct MemberStorage : protected virtual MemberStorageBase
+{
+public:
+    //! @brief returns stored value for provided data member pointer. Throws runtime_error if arg is not registered
+    //! @tparam ClassType data member pointer's class owner
+    //! @tparam MemberType data member pointer's type
+    //! @param arg data member pointer
+    //! @return stored value for provided arg
+    template <typename ClassType, typename MemberType>
+    static const StoredType& get(MemberType ClassType::*arg)
+    {
+        const auto& it = std::find_if(
+            storage.cbegin(), storage.cend(),
+            [arg](const std::pair<MemberTypeBase*, StoredType>& _it) -> bool {
+                if (_it.first->class_hash == typeid(ClassType).hash_code())
+                {
+                    const auto* ref =
+                        static_cast<const Member_t<ClassType, MemberType>*>(
+                            _it.first);
+                    if (ref)
+                        return ref->member == arg;
+                }
+                return false;
+            });
+        if (it != storage.cend())
+            return it->second;
+        else
+            throw std::runtime_error("No storage for " +
+                                     std::string(typeid(ClassType).name()) + " " +
+                                     std::string(typeid(MemberType).name()));
+    }
+
+protected:
+    //! @brief register new data member pointer and it's value under T storage
+    //! @tparam _ClassType 
+    //! @tparam _MemberType 
+    //! @param arg data member pointer
+    //! @param value value
+    //! @return 
+    template <typename _ClassType, typename _MemberType>
+    bool registerMember(_MemberType _ClassType::*arg, const StoredType value)
+    {
+        storage[new Member_t(arg)] = value;
+        return true;
+    }
+
+protected:
+    inline static std::unordered_map<MemberTypeBase*, StoredType, MemberHasher,
+                                     MemberEqual>
+        storage;
+};
---
+++ Serializer.h
@@ -0,0 +1,225 @@
+#pragma once
+
+#include "MemberStorage.h"
+
+#include <nlohmann/json.hpp>
+#include <variant>
+
+#define SERIALIZER_ADD_MEMBER(Class, Member) \
+    Class::registerMember(&Class::Member, #Member);
+
+template <typename T>
+struct is_variant : std::false_type
+{
+};
+
+template <typename... Args>
+struct is_variant<std::variant<Args...>> : std::true_type
+{
+};
+
+template <typename T>
+inline constexpr bool is_variant_v = is_variant<T>::value;
+
+template <typename T>
+struct is_shared_ptr : std::false_type
+{
+};
+
+template <typename T>
+struct is_shared_ptr<std::shared_ptr<T>> : std::true_type
+{
+};
+
+template <typename T>
+inline constexpr bool is_shared_ptr_v = is_shared_ptr<T>::value;
+
+namespace slpr {
+
+class SerializerException : public std::runtime_error
+{
+private:
+    std::string _msg;
+
+public:
+    SerializerException(const char* msg) throw()
+        : std::runtime_error(msg), _msg(msg)
+    {
+    }
+    virtual char const* what() const throw() { return _msg.c_str(); }
+};
+
+template <typename T>
+struct Serializer : public MemberStorage<Serializer<T>, std::string>
+{
+public:
+    //! @brief simple add data member to json file
+    //! @tparam _MemberType data member type 
+    //! @tparam _TargetType specify if underlying type is other than container
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param target target json structure
+    //! @param arg source data member pointer
+    template <typename _MemberType, typename _TargetType = _MemberType,
+              typename _ClassType>
+    inline void to_json_simple(nlohmann::json& target,
+                               const _MemberType _ClassType::*arg) const
+    {
+        Serializer<T>::to_json_simple<_MemberType, _TargetType, _ClassType>(
+            target, arg, dynamic_cast<const _ClassType&>(*this));
+    }
+
+    //! @brief simple read value from json into data member pointer
+    //! @tparam _MemberType data member type
+    //! @tparam _TargetType specify if underlying type is other than container
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param source source json structure
+    //! @param arg target data member pointer
+    template <typename _MemberType, typename _TargetType = _MemberType,
+              typename _ClassType>
+    inline void from_json_simple(const nlohmann::json& source,
+                                 _MemberType _ClassType::*arg)
+    {
+        Serializer<T>::from_json_simple<_MemberType, _TargetType, _ClassType>(
+            source, arg, dynamic_cast<_ClassType&>(*this));
+    }
+
+    //! @brief add add source json struct under data member pointer name into target json struct
+    //! @tparam _MemberType data member type
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param target target json where new property is going to be added
+    //! @param arg data member pointer used to obtain property name
+    //! @param source new json property to be added to target
+    template <typename _MemberType, typename _ClassType>
+    inline void add_json_to_property(nlohmann::json& target,
+                                     _MemberType _ClassType::*arg,
+                                     const nlohmann::json& source) const
+    {
+        try
+        {
+            auto& name = Serializer::get(arg);
+            target[name] = source;
+        }
+        catch (std::exception& e)
+        {
+            handleException(e);
+        }
+    }
+
+    //! @brief from source returns json structure for data member pointer's associated name 
+    //! @tparam _MemberType data member type
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param source source json struct
+    //! @param arg data member pointer used to obtain property name
+    //! @return extracted json structure for data member pointer's property name
+    template <typename _MemberType, typename _ClassType>
+    inline nlohmann::json get_json_for_property(const nlohmann::json& source,
+                                                _MemberType _ClassType::*arg)
+    {
+        try
+        {
+            auto& name = Serializer::get(arg);
+            return source.at(name);
+        }
+        catch (std::exception& e)
+        {
+            handleException(e);
+            return nlohmann::json();
+        }
+    }
+
+    //! @brief simple add data member to json file
+    //! @tparam _MemberType data member type
+    //! @tparam _TargetType specify if underlying type is other than container
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param target target json structure
+    //! @param arg source data member pointer
+    //! @param instance 
+    template <typename _MemberType, typename _TargetType = _MemberType,
+              typename _ClassType>
+    static void to_json_simple(nlohmann::json& target,
+                               const _MemberType _ClassType::*arg,
+                               const _ClassType& instance)
+    {
+        try
+        {
+            auto& name = Serializer::get(arg);
+            target[name] = getMemberRef<_TargetType>(arg, instance);
+        }
+        catch (std::exception& e)
+        {
+            handleException(e);
+        }
+    }
+
+    //! @brief simple read value from json into data member pointer
+    //! @tparam _MemberType data member type
+    //! @tparam _TargetType specify if underlying type is other than container
+    //! @tparam _ClassType class type owning data member pointer
+    //! @param source source json structure
+    //! @param arg target data member pointer
+    //! @param instance 
+    template <typename _MemberType, typename _TargetType = _MemberType,
+              typename _ClassType>
+    static void from_json_simple(const nlohmann::json& source,
+                                 _MemberType _ClassType::*arg,
+                                 _ClassType& instance)
+    {
+        try
+        {
+            auto& name = Serializer::get(arg);
+            source.at(name).get_to(getMemberRef<_TargetType>(arg, instance));
+        }
+        catch (std::exception& e)
+        {
+            handleException(e);
+        }
+    }
+
+public:
+    friend void to_json(nlohmann::json& j, const Serializer<T>& p)
+    {
+        p.to_json(j);
+    }
+    friend void from_json(const nlohmann::json& j, Serializer<T>& p)
+    {
+        p.from_json(j);
+    }
+
+private:
+    template <typename _TargetType, typename _ClassType, typename _MemberType>
+    static const _TargetType& getMemberRef(const _MemberType _ClassType::*arg,
+                                           const _ClassType& instance)
+    {
+        if constexpr (is_variant_v<_MemberType>)
+            return std::get<_TargetType>(instance.*arg);
+        else if constexpr (is_shared_ptr_v<_MemberType>)
+            return *(instance.*arg).get();
+        else
+            return instance.*arg;
+    }
+
+    template <typename _TargetType, typename _ClassType, typename _MemberType>
+    static _TargetType& getMemberRef(_MemberType _ClassType::*arg,
+                                     _ClassType& instance)
+    {
+        if constexpr (is_variant_v<_MemberType>)
+            return std::get<_TargetType>(instance.*arg);
+        else if constexpr (is_shared_ptr_v<_MemberType>)
+            return *(instance.*arg).get();
+        else
+            return instance.*arg;
+    }
+
+    static void handleException(std::exception& e)
+    {
+#ifdef DEBUG
+        std::cout << "[SERIALIZER] " << e.what() << std::endl;
+#endif // DEBUG
+        //throw SerializerException(e.what());
+    }
+
+protected:
+    virtual inline void to_json(nlohmann::json& j) const = 0;
+    virtual inline void from_json(const nlohmann::json& j) = 0;
+};
+} // namespace slpr
