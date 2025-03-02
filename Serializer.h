e1fe32507b19791dc05e49941aecae154aa32884
Bartlomiej Drozd <drozd.bartek96@gmail.com>
2025-03-02 19:58:44 +0100

    add CMakeList and remove old namespace

--- Serializer.h
+++ Serializer.h
@@ -5,61 +5,52 @@
  */
 #pragma once
 
-#include "MemberStorage.h"
-
 #include <nlohmann/json.hpp>
 #include <variant>
+
+#include "MemberStorage.h"
 
 #define SERIALIZER_ADD_MEMBER(Class, Member) \
     Class::registerMember(&Class::Member, #Member);
 
 template <typename T>
-struct is_variant : std::false_type
-{
+struct is_variant : std::false_type {
 };
 
 template <typename... Args>
-struct is_variant<std::variant<Args...>> : std::true_type
-{
+struct is_variant<std::variant<Args...>> : std::true_type {
 };
 
 template <typename T>
 inline constexpr bool is_variant_v = is_variant<T>::value;
 
 template <typename T>
-struct is_shared_ptr : std::false_type
-{
+struct is_shared_ptr : std::false_type {
 };
 
 template <typename T>
-struct is_shared_ptr<std::shared_ptr<T>> : std::true_type
-{
+struct is_shared_ptr<std::shared_ptr<T>> : std::true_type {
 };
 
 template <typename T>
 inline constexpr bool is_shared_ptr_v = is_shared_ptr<T>::value;
 
-namespace slpr {
-
-class SerializerException : public std::runtime_error
-{
-private:
+class SerializerException : public std::runtime_error {
+   private:
     std::string _msg;
 
-public:
+   public:
     SerializerException(const char* msg) throw()
-        : std::runtime_error(msg), _msg(msg)
-    {
+        : std::runtime_error(msg), _msg(msg) {
     }
     virtual char const* what() const throw() { return _msg.c_str(); }
 };
 
 template <typename T>
-struct Serializer : public MemberStorage<Serializer<T>, std::string>
-{
-public:
+struct Serializer : public MemberStorage<Serializer<T>, std::string> {
+   public:
     //! @brief simple add data member to json file
-    //! @tparam _MemberType data member type 
+    //! @tparam _MemberType data member type
     //! @tparam _TargetType specify if underlying type is other than container
     //! @tparam _ClassType class type owning data member pointer
     //! @param target target json structure
@@ -67,8 +58,7 @@ public:
     template <typename _MemberType, typename _TargetType = _MemberType,
               typename _ClassType>
     inline void to_json_simple(nlohmann::json& target,
-                               const _MemberType _ClassType::*arg) const
-    {
+                               const _MemberType _ClassType::* arg) const {
         Serializer<T>::to_json_simple<_MemberType, _TargetType, _ClassType>(
             target, arg, dynamic_cast<const _ClassType&>(*this));
     }
@@ -82,8 +72,7 @@ public:
     template <typename _MemberType, typename _TargetType = _MemberType,
               typename _ClassType>
     inline void from_json_simple(const nlohmann::json& source,
-                                 _MemberType _ClassType::*arg)
-    {
+                                 _MemberType _ClassType::* arg) {
         Serializer<T>::from_json_simple<_MemberType, _TargetType, _ClassType>(
             source, arg, dynamic_cast<_ClassType&>(*this));
     }
@@ -96,21 +85,17 @@ public:
     //! @param source new json property to be added to target
     template <typename _MemberType, typename _ClassType>
     inline void add_json_to_property(nlohmann::json& target,
-                                     _MemberType _ClassType::*arg,
-                                     const nlohmann::json& source) const
-    {
-        try
-        {
+                                     _MemberType _ClassType::* arg,
+                                     const nlohmann::json& source) const {
+        try {
             auto& name = Serializer::get(arg);
             target[name] = source;
-        }
-        catch (std::exception& e)
-        {
+        } catch (std::exception& e) {
             handleException(e);
         }
     }
 
-    //! @brief from source returns json structure for data member pointer's associated name 
+    //! @brief from source returns json structure for data member pointer's associated name
     //! @tparam _MemberType data member type
     //! @tparam _ClassType class type owning data member pointer
     //! @param source source json struct
@@ -118,15 +103,11 @@ public:
     //! @return extracted json structure for data member pointer's property name
     template <typename _MemberType, typename _ClassType>
     inline nlohmann::json get_json_for_property(const nlohmann::json& source,
-                                                _MemberType _ClassType::*arg)
-    {
-        try
-        {
+                                                _MemberType _ClassType::* arg) {
+        try {
             auto& name = Serializer::get(arg);
             return source.at(name);
-        }
-        catch (std::exception& e)
-        {
+        } catch (std::exception& e) {
             handleException(e);
             return nlohmann::json();
         }
@@ -138,20 +119,16 @@ public:
     //! @tparam _ClassType class type owning data member pointer
     //! @param target target json structure
     //! @param arg source data member pointer
-    //! @param instance 
+    //! @param instance
     template <typename _MemberType, typename _TargetType = _MemberType,
               typename _ClassType>
     static void to_json_simple(nlohmann::json& target,
-                               const _MemberType _ClassType::*arg,
-                               const _ClassType& instance)
-    {
-        try
-        {
+                               const _MemberType _ClassType::* arg,
+                               const _ClassType& instance) {
+        try {
             auto& name = Serializer::get(arg);
             target[name] = getMemberRef<_TargetType>(arg, instance);
-        }
-        catch (std::exception& e)
-        {
+        } catch (std::exception& e) {
             handleException(e);
         }
     }
@@ -162,39 +139,32 @@ public:
     //! @tparam _ClassType class type owning data member pointer
     //! @param source source json structure
     //! @param arg target data member pointer
-    //! @param instance 
+    //! @param instance
     template <typename _MemberType, typename _TargetType = _MemberType,
               typename _ClassType>
     static void from_json_simple(const nlohmann::json& source,
-                                 _MemberType _ClassType::*arg,
-                                 _ClassType& instance)
-    {
-        try
-        {
+                                 _MemberType _ClassType::* arg,
+                                 _ClassType& instance) {
+        try {
             auto& name = Serializer::get(arg);
             source.at(name).get_to(getMemberRef<_TargetType>(arg, instance));
-        }
-        catch (std::exception& e)
-        {
+        } catch (std::exception& e) {
             handleException(e);
         }
     }
 
-public:
-    friend void to_json(nlohmann::json& j, const Serializer<T>& p)
-    {
+   public:
+    friend void to_json(nlohmann::json& j, const Serializer<T>& p) {
         p.to_json(j);
     }
-    friend void from_json(const nlohmann::json& j, Serializer<T>& p)
-    {
+    friend void from_json(const nlohmann::json& j, Serializer<T>& p) {
         p.from_json(j);
     }
 
-private:
+   private:
     template <typename _TargetType, typename _ClassType, typename _MemberType>
-    static const _TargetType& getMemberRef(const _MemberType _ClassType::*arg,
-                                           const _ClassType& instance)
-    {
+    static const _TargetType& getMemberRef(const _MemberType _ClassType::* arg,
+                                           const _ClassType& instance) {
         if constexpr (is_variant_v<_MemberType>)
             return std::get<_TargetType>(instance.*arg);
         else if constexpr (is_shared_ptr_v<_MemberType>)
@@ -204,9 +174,8 @@ private:
     }
 
     template <typename _TargetType, typename _ClassType, typename _MemberType>
-    static _TargetType& getMemberRef(_MemberType _ClassType::*arg,
-                                     _ClassType& instance)
-    {
+    static _TargetType& getMemberRef(_MemberType _ClassType::* arg,
+                                     _ClassType& instance) {
         if constexpr (is_variant_v<_MemberType>)
             return std::get<_TargetType>(instance.*arg);
         else if constexpr (is_shared_ptr_v<_MemberType>)
@@ -215,16 +184,14 @@ private:
             return instance.*arg;
     }
 
-    static void handleException(std::exception& e)
-    {
+    static void handleException(std::exception& e) {
 #ifdef DEBUG
         std::cout << "[SERIALIZER] " << e.what() << std::endl;
-#endif // DEBUG
-        //throw SerializerException(e.what());
+#endif  // DEBUG
+        // throw SerializerException(e.what());
     }
 
-protected:
+   protected:
     virtual inline void to_json(nlohmann::json& j) const = 0;
     virtual inline void from_json(const nlohmann::json& j) = 0;
 };
-} // namespace slpr
